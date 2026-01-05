80deab556e219ac1e6061bd3030ad958f7efdbb8
ChrisLam790 <christopherlam790@gmail.com>
2026-01-05 00:22:30 -0500

    complete transofrm of raw data

--- src/transform_raw_data.py
+++ src/transform_raw_data.py
@@ -36,6 +36,13 @@ def add_day_of_week_col(df):
     return df
 
 """
+=========
+ROUNDING
+
+"""
+
+
+"""
 Round to 2 decimal places via standard rounding
 """
 def add_true_rounded_close(df):
@@ -49,6 +56,22 @@ Round to nearest dollar via standard rounding
 """
 def add_true_dollar_rounded_close(df):
     df["true_dollar_rounded_close"] = df["close"].round(0)
+    
+    return df
+
+"""
+Round to nearest .5 via standard rounding
+"""
+def add_true_point_5_rounded_close(df):
+    df["true_point_5_rounded_close"] = (df["close"] * 2).round() / 2
+    
+    return df
+
+"""
+Round to nearest .25 via standard rounding
+"""
+def add_true_point_25_rounded_close(df):
+    df["true_point_25_rounded_close"] = (df["close"] * 4).round() / 4
     
     return df
 
@@ -67,6 +90,15 @@ def add_ceil_rounded_close(df):
     df["ceil_rounded_close"] = df["close"].apply(np.ceil)
     
     return df 
+  
+  
+    
+"""
+===================
+TRUE MULTIPLES OF X
+
+"""    
+
     
 """
 Check if dollar rounded is true multiple of 5
@@ -78,37 +110,78 @@ def add_is_dollar_rounded_true_multiple_of_5(df):
     return df
 
 """
-Check if dollar rounded is true multiple of 2.5
+Check if dollar rounded is true multiple of 10
 """
-def add_is_dollar_rounded_true_multiple_of_2_point_5(df):
+def add_is_dollar_rounded_true_multiple_of_10(df):
     
-    df["is_dollar_rounded_true_multiple_of_2_point_5"] = [1 if close % 2.5 == 0 else 0 for close in df["true_dollar_rounded_close"]]
+    df["is_dollar_rounded_true_multiple_of_10"] = [1 if close % 10 == 0 else 0 for close in df["true_dollar_rounded_close"]]
     
     return df
 
+def add_is_point_5_rounded_true_multiple_of_1(df):
+    
+    df["is_point_5_rounded_true_multiple_of_1"] = [1 if close % 1 == 0 else 0 for close in df["true_point_5_rounded_close"]] 
+    return df
 
+def add_is_point_25_rounded_true_multiple_of_1(df):
+    
+    df["is_point_25_rounded_true_multiple_of_1"] = [1 if close % 1 == 0 else 0 for close in df["true_point_25_rounded_close"]] 
+    return df
+
+
+"""
+========
+TOLERANCE MULTIPLE OF X
+
+"""
+
+"""
+Check if dollar rounded is near multiple of 5 tolerance 1
+"""
 def add_is_dollar_rounded_tolerance_1_multiple_of_5(df):
     
     df["is_dollar_rounded_tolerance_1_multiple_of_5"] = [1 if close % 5 in [0, 1, 4] else 0 for close in df["true_dollar_rounded_close"]]
     
     return df
 
+"""
+Check if dollar rounded is near multiple of 10 tolerance 1
+"""
+def add_is_dollar_rounded_tolerance_1_multiple_of_10(df):
+    
+    df["is_dollar_rounded_tolerance_1_multiple_of_10"] = [1 if close % 10 in [0, 1, 9] else 0 for close in df["true_dollar_rounded_close"]]
+    
+    return df
+
+
 
 
 def transform_raw_date(ticker):
+    # Get data
     df = get_raw_df_from_sql(ticker=ticker) 
+    
     df = add_day_of_week_col(df=df)
     
+    # Add rounded cols
     df = add_true_rounded_close(df=df)
     df = add_true_dollar_rounded_close(df=df)
+    df = add_true_point_5_rounded_close(df=df)
+    df = add_true_point_25_rounded_close(df=df)
     df = add_floor_rounded_close(df=df)
     df = add_ceil_rounded_close(df=df)
     
+    # Add is true multiple
     df = add_is_dollar_rounded_true_multiple_of_5(df=df)
-    df = add_is_dollar_rounded_true_multiple_of_2_point_5(df=df)
-    df = add_is_dollar_rounded_tolerance_1_multiple_of_5(df=df)
+    df = add_is_dollar_rounded_true_multiple_of_10(df=df)
+    df = add_is_point_5_rounded_true_multiple_of_1(df=df)
+    df = add_is_point_25_rounded_true_multiple_of_1(df=df)
     
-    print(df[["close","true_dollar_rounded_close", "is_dollar_rounded_tolerance_1_multiple_of_5"]]) 
+    # Add is tolerance multiple
+    df = add_is_dollar_rounded_tolerance_1_multiple_of_5(df=df)
+    df = add_is_dollar_rounded_tolerance_1_multiple_of_10(df=df)
+    
+        
+    return df 
     
 
 
@@ -119,5 +192,8 @@ TESTING AREA
 if __name__ == "__main__":
     
     
-    transform_raw_date("^SPX")
+    transform_raw_date("SPY")
+    
+    
+        
     print("TESTING COMPLETE")
